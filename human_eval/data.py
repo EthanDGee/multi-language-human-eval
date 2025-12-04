dc0bc9b28b73df09d044bb98fb9ffc0bc2dd3235
EthanDGee <workethangee@gmail.com>
2025-12-04 10:59:25 -0700

    Updated evaluation to use translated human eval
    - refactored HUMAN_EVAL -> TRANSLATED_HUMAN_EVAL
    - updated formatting

--- human_eval/data.py
+++ human_eval/data.py
@@ -5,20 +5,20 @@ import os
 
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
-HUMAN_EVAL = os.path.join(ROOT, "..", "data", "HumanEval.jsonl.gz")
+TRANSLATED_HUMAN_EVAL = os.path.join(ROOT, "..", "data", "TranslatedHumanEval.jsonl.gz")
 
 
-def read_problems(evalset_file: str = HUMAN_EVAL) -> Dict[str, Dict]:
+def read_problems(evalset_file: str = TRANSLATED_HUMAN_EVAL) -> Dict[str, Dict]:
     return {task["task_id"]: task for task in stream_jsonl(evalset_file)}
 
 
-def stream_jsonl(filename: str) -> Iterable[Dict]:
+def stream_jsonl(filename: str = TRANSLATED_HUMAN_EVAL):
     """
     Parses each jsonl line and yields it as a dictionary
     """
     if filename.endswith(".gz"):
         with open(filename, "rb") as gzfp:
-            with gzip.open(gzfp, 'rt') as fp:
+            with gzip.open(gzfp, "rt") as fp:
                 for line in fp:
                     if any(not x.isspace() for x in line):
                         yield json.loads(line)
@@ -34,16 +34,16 @@ def write_jsonl(filename: str, data: Iterable[Dict], append: bool = False):
     Writes an iterable of dictionaries to jsonl
     """
     if append:
-        mode = 'ab'
+        mode = "ab"
     else:
-        mode = 'wb'
+        mode = "wb"
     filename = os.path.expanduser(filename)
     if filename.endswith(".gz"):
         with open(filename, mode) as fp:
-            with gzip.GzipFile(fileobj=fp, mode='wb') as gzfp:
+            with gzip.GzipFile(fileobj=fp, mode="wb") as gzfp:
                 for x in data:
-                    gzfp.write((json.dumps(x) + "\n").encode('utf-8'))
+                    gzfp.write((json.dumps(x) + "\n").encode("utf-8"))
     else:
         with open(filename, mode) as fp:
             for x in data:
-                fp.write((json.dumps(x) + "\n").encode('utf-8'))
+                fp.write((json.dumps(x) + "\n").encode("utf-8"))
--- human_eval/evaluation.py
+++ human_eval/evaluation.py
@@ -1,19 +1,22 @@
-from collections import defaultdict, Counter
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import List, Union, Iterable, Dict
+from typing import List, Union, Iterable, Dict, Optional
 import itertools
 
 import numpy as np
 import tqdm
 
-from human_eval.data import HUMAN_EVAL, read_problems, stream_jsonl, write_jsonl
+from human_eval.data import (
+    TRANSLATED_HUMAN_EVAL,
+    read_problems,
+    stream_jsonl,
+    write_jsonl,
+)
 from human_eval.execution import check_correctness
 
 
 def estimate_pass_at_k(
     num_samples: Union[int, List[int], np.ndarray],
     num_correct: Union[List[int], np.ndarray],
-    k: int
+    k: int,
 ) -> np.ndarray:
     """
     Estimates pass@k of each problem and returns them in an array.
@@ -33,7 +36,9 @@ def estimate_pass_at_k(
         assert len(num_samples) == len(num_correct)
         num_samples_it = iter(num_samples)
 
-    return np.array([estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)])
+    return np.array(
+        [estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)]
+    )
 
 
 def evaluate_functional_correctness(
@@ -41,7 +46,8 @@ def evaluate_functional_correctness(
     k: List[int] = [1, 10, 100],
     n_workers: int = 4,
     timeout: float = 3.0,
-    problem_file: str = HUMAN_EVAL,
+    problem_file: str = TRANSLATED_HUMAN_EVAL,
+    language: Optional[str] = "english",
 ):
     """
     Evaluates the functional correctness of generated samples, and writes
@@ -52,7 +58,6 @@ def evaluate_functional_correctness(
 
     # Check the generated samples against test suites.
     with ThreadPoolExecutor(max_workers=n_workers) as executor:
-
         futures = []
         completion_id = Counter()
         n_samples = 0
@@ -62,7 +67,11 @@ def evaluate_functional_correctness(
         for sample in tqdm.tqdm(stream_jsonl(sample_file)):
             task_id = sample["task_id"]
             completion = sample["completion"]
-            args = (problems[task_id], completion, timeout, completion_id[task_id])
+            problem = problems[task_id]
+            if "prompts" in problem and language in problem["prompts"]:
+                problem["prompt"] = problem["prompts"][language]
+
+            args = (problem, completion, timeout, completion_id[task_id])
             future = executor.submit(check_correctness, *args)
             futures.append(future)
             completion_id[task_id] += 1
@@ -86,8 +95,11 @@ def evaluate_functional_correctness(
     correct = np.array(correct)
 
     ks = k
-    pass_at_k = {f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
-                 for k in ks if (total >= k).all()}
+    pass_at_k = {
+        f"pass@{k}": estimate_pass_at_k(total, correct, k).mean()
+        for k in ks
+        if (total >= k).all()
+    }
 
     # Finally, save the results in one file:
     def combine_results():
